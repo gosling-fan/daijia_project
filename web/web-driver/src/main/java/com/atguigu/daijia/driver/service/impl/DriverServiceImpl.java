858ad67653e680435b90c5dea7ebdaf03366d493
gosling-fan <dzp183ban@gmail.com>
2024-07-24 15:23:13 +0800

    司机端登录以及信息显示功能开发

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverServiceImpl.java
@@ -1,15 +1,46 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.driver.client.DriverInfoFeignClient;
 import com.atguigu.daijia.driver.service.DriverService;
+import com.atguigu.daijia.model.vo.driver.DriverLoginVo;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class DriverServiceImpl implements DriverService {
+    @Autowired
+    private DriverInfoFeignClient driverInfoFeignClient;
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Override
+    public String login(String code) {
+        Result<Long> result = driverInfoFeignClient.login(code);
+        Integer resultCode = result.getCode();
+        if(resultCode!=200){
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+        Long driverId = result.getData();
+        String token = UUID.randomUUID().toString().replaceAll("-", "");
+        redisTemplate.opsForValue().set(RedisConstant.USER_LOGIN_KEY_PREFIX + token, driverId.toString(),
+                RedisConstant.USER_LOGIN_KEY_TIMEOUT, TimeUnit.SECONDS);
+        return token;
 
+    }
 
+    @Override
+    public DriverLoginVo getDriverLoginInfo(Long driverId) {
+        return  driverInfoFeignClient.getDriverLoginInfo(driverId).getData();
+    }
 }
